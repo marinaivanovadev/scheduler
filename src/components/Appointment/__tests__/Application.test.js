41ece2594d447e65a3fc82a9ad9ec7a0fafefaa8
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-10-02 22:21:40 -0700

    Integration testing

--- src/components/Appointment/__tests__/Application.test.js
+++ src/components/Appointment/__tests__/Application.test.js
@@ -1,15 +1,50 @@
 import React from "react";
-import { render, waitForElement, fireEvent } from "@testing-library/react";
+import { render, waitForElement, fireEvent, act } from "@testing-library/react";
+import {
+  getByText,
+  prettyDOM,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 describe("Appointment", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
-    const { getByText } = render(<Application />);
+  it("changes the schedule when a new day is selected", async () => {
+    await act(async () => {
+      const { getByText } = render(<Application />);
 
-    return waitForElement(() => getByText("Monday")).then(() => {
+      await waitForElement(() => getByText("Monday"));
+
       fireEvent.click(getByText("Tuesday"));
+
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
   });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    await act(async () => {
+      const { container } = render(<Application />);
+
+      await waitForElement(() => getByText(container, "Archie Cohen"));
+
+        const appointments = getAllByTestId(container, "appointment");
+        const appointment = appointments[0];
+      
+
+      fireEvent.click(getByAltText(appointment, "Add"));
+
+      fireEvent.change(
+        getByPlaceholderText(appointment, /enter student name/i),
+        {
+          target: { value: "Lydia Miller-Jones" }
+        });
+      fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+      fireEvent.click(getByText(appointment, "Save"));
+
+      console.log(prettyDOM(appointment));
+    });
   });
+});
